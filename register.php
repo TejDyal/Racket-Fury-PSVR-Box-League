b99263470e98d7174fd1508e1a8603b7c7731fe2
Tej Dyal <tej.dyal@yahoo.com>
2020-08-23 21:38:00 +0100

    implemented reg form submittion

--- register.php
+++ register.php
@@ -21,21 +21,19 @@
     //declaring variables to prevent errors and hacking
     $psn = $email = $confirmEmail = $enterLeague = $password = $confirmPwd = $dateOfReg = "";
     $errorArray = array();
-    $serverIds = array();
     $serverNames = array();
 
-    //fetch Racket Fury server list from db
+    //fetch Racket Fury server list from db to show on form
     $result = mysqli_query($conn, "SELECT Server_id, serverName FROM server");
     if (mysqli_num_rows($result) > 0) {
         while ($row = mysqli_fetch_assoc($result)) {
-            array_push($serverIds, $row["Server_id"]);
             array_push($serverNames, $row["serverName"]);
         }
     }
 
 
 
-    //validating and stripping inputs on form
+    //validating inputs on form and prepping the values before sending to DB
     if (isset($_POST['regBtn'])) {
 
         $psn = strip_tags($_POST['psn']);
@@ -52,7 +50,8 @@
             array_push($errorArray, "Invalid PSN ID<br>");
         }
 
-
+        
+        $serverNameSelected = $_POST['server'];
 
 
         $email = $_POST['email'];
@@ -76,6 +75,10 @@
             array_push($errorArray, "emails don't match<br>");
         }
 
+        $enterLeague = $_POST['enterLeague'];
+
+        $selfRating = $_POST['selfRating'];
+
         $password = strip_tags($_POST['password']);
         $confirmPwd = strip_tags($_POST['confirmPwd']);
 
@@ -91,27 +94,41 @@
             }
         }
 
-        $enterLeague = $_POST['enterLeague'];
-
-        $dateOfReg = date("Y-m-d");
-    }
-
-    // submit form to the database
-    if (empty($errorArray)) {
-        //encrypt password
-        $password = md5($password);
-
-        // assign a default profile pic
-        do {
-            $pics = scandir("assets/images/profile_pics/defaults/");
-            $rand = rand(2, count($pics) - 1);
-            $profilePic = $pics[$rand];
-        } while (!preg_match("/\.png$/i",$profilePic));
-
-        //$serverKey = key()
-        //$query = mysqli_query($conn, "");
         
+        // on no errors after sbmitting form, prep other information before sending to DB
+        if (empty($errorArray)) {
+
+            $dateOfReg = date("Y-m-d");
+        
+            //encrypt password
+            $password = md5($password);
+
+            // assign a default profile pic
+            do {
+                $pics = scandir("assets/images/profile_pics/defaults/");
+                $rand = rand(2, count($pics) - 1);
+                $profilePic = $pics[$rand];
+            } while (!preg_match("/\.png$/i", $profilePic));
+            echo $profilePic;
+
+            //match server id rom DB for the servername selected
+            $result = mysqli_query($conn, "SELECT Server_id FROM server WHERE serverName = ('$serverNameSelected')");
+            $row = mysqli_fetch_assoc($result);
+            $serverId = $row["Server_id"];
+        }
+
+        //submit form data to DB
+        $result = mysqli_query($conn, "INSERT INTO player (`PSN_id`, `Email`, `activeInLeague`, `enterInNextLeague`, `standard`, `Server_id`, `dateRegistered`, `pwd`, `profilePic`) VALUES ('$psn','$email', '0', '$enterLeague', '$selfRating', '$serverId', '$dateOfReg', '$password', '$profilePic')");
+
+        if (!$result) {
+            print_r(mysqli_error_list($conn));
+        }
+        else{ echo "Success"; }
+
+
     }
+
+
 
     ?>
 
@@ -124,16 +141,18 @@
             if (in_array("Invalid PSN ID<br>", $errorArray)) echo "Invalid PSN ID<br>";
             else if (in_array("this PSN ID aready exists<br>", $errorArray)) echo "this PSN ID aready exists<br>"
             ?>
-            <!-- TODO: need code here to test if player already exists on db (DONE), and if so, test if has a login. If no login, then continue with form.  If user has a login then go to login form. -->
+            <!-- TODO: need code here to test if player already exists on db (DONE), and if so, test if has a login. If no login, then continue with form.  If user has a login then go to login form. (Login form yet to be implemented) -->
         </div>
 
+        <!-- this is a dropdown select list cycles through the servers list array -->
         <div class="box regb" id="serverSelect">
             <label for="server">Select your local server:</label>
             <select name="server" required>
                 <?php
                 foreach ($serverNames as $server) {
                 ?>
-                    <option value="<?php echo str_replace(' ', '', strtolower($server)); ?>"><?php echo $server; ?> </option>
+                    <option value="<?php echo $server; ?>" <?php if (isset($_POST['server']) && $_POST['server'] == $server)
+                                                                echo 'selected= "selected"' ?>><?php echo $server; ?> </option>
                 <?php
                 }
                 ?>
@@ -158,19 +177,33 @@
         <div class="box rege" id="enterLeague">
             <label for="enterLeague">Would you like to enter yourself into next season's league? </label>
             <select name="enterLeague" required>
-                <option value="yes" <?php if (isset($_POST['enterLeague']) && $_POST['enterLeague'] == 'yes')
+                <option value=1 <?php if (isset($_POST['enterLeague']) && $_POST['enterLeague'] == 1)
                                         echo 'selected= "selected"' ?>>Yes please!</option>
-                <option value="no" <?php if (isset($_POST['enterLeague']) && $_POST['enterLeague'] == 'no')
+                <option value=0 <?php if (isset($_POST['enterLeague']) && $_POST['enterLeague'] == 0)
                                         echo 'selected= "selected"' ?>>Not right now, but maybe later!</option>
             </select>
         </div>
 
-        <div class="box regf" id="password">
+        <div class="box regf" id="selfRating">
+            <label for="selfRating">How do you rate your playing ability?</label>
+            <select name="selfRating" required>
+                <option value="Beginner" <?php if (isset($_POST['selfRating']) && $_POST['selfRating'] == 'Beginner')
+                                        echo 'selected= "selected"' ?>>Beginner!</option>
+                <option value="Average" <?php if (isset($_POST['selfRating']) && $_POST['selfRating'] == 'Average')
+                                        echo 'selected= "selected"' ?>>Average</option>
+                <option value="Good" <?php if (isset($_POST['selfRating']) && $_POST['selfRating'] == 'Good')
+                                        echo 'selected= "selected"' ?>>Good</option>
+
+
+            </select>
+        </div>
+
+        <div class="box regg" id="password">
             <label for="password">Create a password</label>
             <input type="password" name="password" required>
         </div>
 
-        <div class="box regg" id="confirmPwd">
+        <div class="box regh" id="confirmPwd">
             <label for="confirmPwd">Confirm your new password</label>
             <input type="password" name="confirmPwd" required>
             <?php
@@ -180,7 +213,7 @@
             ?>
         </div>
 
-        <div class="box regh" id="submit">
+        <div class="box regi" id="submit">
             <label for="submit">Submit Form</label>
             <input type="submit" name="regBtn" value="register">
         </div>
