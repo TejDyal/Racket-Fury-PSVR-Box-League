647453f3c73becf83968624e9dfb6c0f7e598afb
Tej Dyal <tej.dyal@yahoo.com>
2020-08-29 02:23:47 +0100

    pseudo code for first season setup sql procedure

--- new_season.php
+++ new_season.php
@@ -17,33 +17,34 @@ add new season number to season table if it doesn't exist.
 
 First sesason players sorting into divisions will be based on self assessment but future seasons will be based on previous season performance and self assessment for new players.
 
+
+[
 First season sort:
 Add new season number into season table if not existing. (sql proc done)
 select all players (player_id) of specific league ordered by self rating, starting from 1 (Good) to 3 (beginner). (sql proc done)
-Store sequence of player_ids result into a new table called division_sorting
-set noOfDivs (in this league, in this season) to number of player_ids divided by 5 rounded up to nearest integer
-Set CurrentMaxDiv to the highest div_id number in division table
+Store sequence of player_ids result into a new table called Sorted (sql proc done)
+
+set noOfDivs (in this league, in this season) to number of player_ids divided by 5 rounded up to nearest integer {sql proc done)}
+
+Set CurrentMaxDiv to the highest div_id number in division table (sql proc done)
 while noOfDivs greater than CurrentMaxDiv Do:
-    add new row with value: CurrentMaxDiv+1 into division table
+    add new row with value: CurrentMaxDiv+1 into Sorted
     increment CurrentMaxDiv
-end while
+end while                     (sql Proc done)
     
-set div = 1
-set divplayers = 0
-for each player id in division_sorting
-  add a new row to division_player with values: league, season, div, player_id
-  increment divplayers
+set divCount = 1
+set divPlayerscount = 0
+for each player id row in Sorted
+  update columns: league, season, division_id
+  increment divPlayersCount
   if divplayers = 5 then
-    increment div
-    reset divplayers to 0
-  
+    increment divCount
+    reset divPlayersCount to 0
+    
+append sorted rows to division_player table.   (sql Proc done)
 
 
-
-{loop through each player and assign them to division starting from division 1 then incrementing the division number after every 5 players}
-
-
-
+]
 
 
 
